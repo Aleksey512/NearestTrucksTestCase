7d21fa8449ea4267d17f04958d995ca909c8e2eb
Aleksey Yakunin <110282089+Aleksey512@users.noreply.github.com>
2023-05-26 09:51:35 +0300

    Update entrypoint.prod.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -9,3 +9,5 @@ python manage.py collectstatic --noinput
 python start_app.py
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@mail.ru', 'admin')" | python manage.py shell
+
+gunicorn --bind 0.0.0.0:5858 NearestTrucksTestCase.wsgi
